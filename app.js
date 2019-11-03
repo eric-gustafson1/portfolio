4fc1ffcf114bebe9b3b417dc710caf8d32b7f70f
Eric Gustafson <weuxfya@gmail.com>
2019-11-03 09:02:59 -0700

    Add Resume page, work on style.

---
+++ app.js
@@ -0,0 +1,6 @@
+// $(".nav-link").on("click", function(e) {
+//   console.log($(this));
+//   $(".nav-link").removeClass("text-success");
+//   $(this).addClass("text-success");
+//   //   e.preventDefault();
+// });
